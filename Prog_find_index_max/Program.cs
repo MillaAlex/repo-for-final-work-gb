bf54ea229abe4db984bf7639b7cb8505c91bb5c2
Lyudmila <liudmila.k1610@gmail.com>
2023-02-28 22:12:51 +0300

    entered data for prog find index of max

---
+++ Prog_find_index_max/Program.cs
@@ -0,0 +1,8 @@
+﻿// Program for search of index of the max value in array
+
+int[] numbers = new int[6] {1, 8, 3, 9, 12, 16};
+
+int currentIndex = 0;
+int maxNumberIndex = 0;
+
+int max = numbers[0];
