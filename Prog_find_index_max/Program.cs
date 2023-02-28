e6bada6b3ccdc64880ff2f339122e38f6db863e2
Lyudmila <liudmila.k1610@gmail.com>
2023-02-28 22:28:12 +0300

    added condition for prog to find index of max

--- Prog_find_index_max/Program.cs
+++ Prog_find_index_max/Program.cs
@@ -6,3 +6,14 @@ int currentIndex = 0;
 int maxNumberIndex = 0;
 
 int max = numbers[0];
+
+while (currentIndex < numbers.Length)
+{
+    if (numbers[currentIndex] > max) 
+    {
+        max = numbers[currentIndex];
+        maxNumberIndex = currentIndex;
+    }
+    else currentIndex++;
+}
+Console.Write($"Index of max value is {maxNumberIndex}; max value is {max}");
